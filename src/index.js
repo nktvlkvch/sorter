7d191237fad0e2a557dffc6146dec03c058fd19c
Nikita Valkevich <n.valkevich@icloud.com>
2018-09-20 20:56:10 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,26 +1,39 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.arr = [];
+    this.arrLength = 0;
+    this.compareFunction = function(a,b){
+      return a - b;
+    }
+
   }
 
   add(element) {
     // your implementation
+    this.arr.push(element);
+    this.arrLength++;
   }
 
   at(index) {
     // your implementation
+    return this.arr[index];
   }
 
   get length() {
     // your implementation
+    return this.arrLength;;
   }
 
   toArray() {
     // your implementation
+    return this.arr;
+
   }
 
   sort(indices) {
     // your implementation
+
   }
 
   setComparator(compareFunction) {
